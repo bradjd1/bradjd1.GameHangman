23e34909781dab4b85abe1baaef02bbf9e7a9ea5
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-07-29 18:05:41 -0500

    saving at end of day

--- script.js
+++ script.js
@@ -3,8 +3,25 @@
 let possibleWords = ['test','one','two','three', 'watermelon'];
 //the chosen word for the user to have to guess
 let chosenWord = '';
+//get and define objects from screen
+let easyBtn = document.querySelector('.easyButton');
+let harderBtn = document.querySelector('.harderButton');
+let displayGamesPlayed = document.querySelector('.gamesPlayed');
+let displayGamesWon = document.querySelector('.gamesWon');
+let displayWinPct = document.querySelector('.winPct');
+let displayWord = document.querySelector('.word');
+let disAlphabetBtns = document.querySelectorAll('.alphabetBtns')
+
+//declare variables
+let difficultyLevel = 'easy';
+let wrongGuess = 0;
+let gamesPlayed = 0;
+let gamesWon = 0;
+let winPct = 0;
 
 function checkLetter () {
+    console.log('in check letter');
+    console.log(disAlphabetBtns[0].children[0]);
     // get letter user picked
     // increment number wrong guesses
     // if easy button, mark letter so can't be picked again
@@ -23,27 +40,48 @@ function checkLetter () {
 }
 
 function easy() {
-    //set difficultyLevel = easy
-    //call reset game
+    difficultyLevel = 'easy';
+    resetGame();
 }
 
 function harder() {
-    //set difficultyLevel = harder
-    //call reset game
+    difficultyLevel = 'harder'
+    resetGame();
 }
 
 function getWord() {
     //get a word from possibleWords
-    //incr games played
+    let word = possibleWords[Math.floor(Math.random() * possibleWords.length)];
+    gamesPlayed++;
     //put word on screen
+    let placeholder = [];
+    for (let i = 0; i < word.length; i++) {
+        placeholder.push('_ ');
+  //      addPlaceholder.classList.add();
+    }
+    let sPlaceholder = placeholder.toString();
+    sPlaceholder = sPlaceholder.replace(/,/g,"");
+    let addPlaceholder = document.createElement('div');
+    addPlaceholder.innerHTML = `<div>${sPlaceholder}</div>`
+    displayWord.appendChild(addPlaceholder);
+    console.log(word, addPlaceholder);
+}
+
+function populateLetters() {
+    //set all letters to be selectable
 }
 
 function resetGame() {
-    //set wrong guess = 0
-    //call get word
-    //increment games played
+    displayWord.innerHTML = "";
+    wrongGuess = 0;
+    getWord();
     //populate letters
 }
-//event listener on easy button - call easy
-//event Listener on Harder button - call harder
-//event Listener on letters - call check letter
+//event listener for easy/harder buttons
+easyBtn.addEventListener('click',easy);
+harderBtn.addEventListener('click',harder);
+
+//event Listener on alphabet buttons - call check letter
+for (let i = 0; i < 26; i++){
+//disAlphabetBtns.addEventListener('click',checkLetter);
+}
